1b578f4085efd38a32d7d6f6a9b928b00b490ed7
sayed725 <ssayed72533@gmail.com>
2024-11-04 07:07:55 +0600

    added initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1 className='text-4xl bg-yellow-600'>Vite + React</h1>
+      <button className="btn btn-accent">Accent</button>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,9 @@
+const Root = () => {
+    return (
+        <div>
+            <h2 className="text-4xl font-bold">hi from root</h2>
+        </div>
+    );
+};
+
+export default Root;
